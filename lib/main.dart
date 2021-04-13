c32cd0b90ff0b6170b354b37218d813399bdfae0
vitabinary <vitaassetbinary@gmail.com>
2021-04-13 14:52:35 +0700

    change icon

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My web',
+      title: 'Vitabinary',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: PrimaryColor,
